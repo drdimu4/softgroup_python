ebac1b363105da6a90d1bbc67334250d7d636b38
Dmytro Levytkyi <drdimu4@gmail.com>
2017-03-16 21:46:48 +0200

    1_4

---
+++ home_tasks/1_4.py
@@ -0,0 +1,53 @@
+def filter_email(emails):
+    email=[]
+    for mail in emails:
+        if '@' in mail:
+            name = mail[0:(mail.find('@'))]
+            flag = True
+            for i in name:
+                if not(i.isalnum() or '_' in i):
+                    flag = False
+            if flag==True:
+                dom = mail[(mail.find('@')):]
+                flag = True
+                for i in dom:
+                    if not(i.isalnum() or '.' in i):
+                        print(i)
+                if flag==True:
+                    dom = dom.split('.')
+                for st in dom:
+                    if not st == '':
+                        continue
+                else:
+                    continue
+                email.append(mail)
+    return email
+
+def fil(emails):
+    res = []
+    for email in emails:
+        mail = email.partition('@')
+        flag = True
+        for i in mail[0]:
+            if not(i.isalnum() or '_' in i):
+                flag = False
+        if flag == False:
+            continue
+
+        flag = True
+        for i in mail[2]:
+             if not (i.isalnum() or '.' in i):
+                flag = False
+
+        if flag == False:
+            continue
+        else:
+            dom = mail[2].split('.')
+
+        if len(dom)>2 and (not '' in dom):
+            res.append(email)
+    return res
+
+emails = ['ab/c@gmail.com.ua', '*@ank.com', '_ny@us.gov.us', 'z@b.k']
+
+print(fil(emails))
