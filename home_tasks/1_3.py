98169e90e6d8a9dcb531c926248702066415f2bc
Dmytro Levytkyi <drdimu4@gmail.com>
2017-03-18 11:49:23 +0200

    right?

--- home_tasks/1_3.py
+++ home_tasks/1_3.py
@@ -1,19 +1,21 @@
-def decorator(function):
-    def wrapper(*args, **kwargs):
-        try:
-            function(*args, **kwargs)
-        except ZeroDivisionError as detail:
-            print('Exception occurred in func:',detail)
-            print('Input args:',*args)
-            print('Input kwargs {}'.format(kwargs))
-            return None
-        except Exception as det:
-            print(det)
-        else:
-            return function(*args,**kwargs)
-    return wrapper
+def decorator_maker():
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            try:
+                function(*args, **kwargs)
+            except ZeroDivisionError as detail:
+                print('Exception occurred in func:',detail)
+                print('Input args:',*args)
+                print('Input kwargs {}'.format(kwargs))
+                return None
+            except Exception as det:
+                print(det)
+            else:
+                return function(*args,**kwargs)
+        return wrapper
+    return decorator
 
-@decorator
+@decorator_maker()
 def func(x, y, **kwargs):
     return x / y
 
