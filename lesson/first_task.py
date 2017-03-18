d1be590415c9c9d94a20955c0b040ceec385ddb2
Dmytro Levytkyi <drdimu4@gmail.com>
2017-03-18 15:44:11 +0200

    lesson

---
+++ lesson/first_task.py
@@ -0,0 +1,21 @@
+class Lists:
+    def __init__(self):
+        self.a = []
+
+    def conv(self,list):
+        for each in list:
+            if isinstance(each,int):
+                self.a.append(each)
+            else:
+                self.conv(each)
+
+    def print(self):
+        print(self.a)
+
+
+
+l = Lists()
+
+l.conv([1,2,3,5,[6,7,[8,9]]])
+l.print()
+
